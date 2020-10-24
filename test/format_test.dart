2f297b319298909087c21d6d862a4f1ebfc8f4a8
Guillaume Roux <guillaume2.roux@epitech.eu>
2020-10-24 21:45:29 +0200

    make package compatible with dart core

--- test/format_test.dart
+++ test/format_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:icalendar_parser/icalendar_parser.dart';
 import 'package:icalendar_parser/src/exceptions/icalendar_exception.dart';
+import 'package:test/test.dart';
 
 main() {
   final _valid =
@@ -10,15 +10,14 @@ main() {
   final _noCalendarEnd =
       'BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//hacksw/handcal//NONSGML v1.0//EN\nBEGIN:VEVENT\nUID:uid1@example.com\nDTSTAMP:19970714T170000Z\nORGANIZER;CN=John Doe:MAILTO:john.doe@example.com\nDTSTART:19970714T170000Z\nDTEND:19970715T035959Z\nSUMMARY:Bastille Day Party\nGEO:48.85299;2.36885\nEND:VEVENT';
 
-  TestWidgetsFlutterBinding.ensureInitialized();
   test('Missing BEGIN:VCALENDAR', () {
     expect(() => ICalendar.fromString(_noCalendarBegin),
-        throwsA(isInstanceOf<ICalendarBeginException>()));
+        throwsA(isA<ICalendarBeginException>()));
   });
 
   test('Missing END:VCALENDAR', () {
     expect(() => ICalendar.fromString(_noCalendarEnd),
-        throwsA(isInstanceOf<ICalendarEndException>()));
+        throwsA(isA<ICalendarEndException>()));
   });
 
   test('Valid calendar', () {
