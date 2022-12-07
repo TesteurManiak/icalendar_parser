f1482386e70fed42a5359393efa727b95f9552c2
Guillaume Roux <rouxguillaume8@gmail.com>
2022-12-07 18:07:14 +0100

    created unit test and fixed test file

--- test/issue_43_test.dart
+++ test/issue_43_test.dart
@@ -1,4 +1,5 @@
 import 'package:icalendar_parser/src/model/icalendar.dart';
+import 'package:icalendar_parser/src/model/ics_datetime.dart';
 import 'package:test/test.dart';
 
 import 'test_utils.dart';
@@ -8,6 +9,15 @@ void main() {
     test('Multi-line EXDATE', () {
       final eventText = readFileLines('multi_line_exdate.ics');
       final iCal = ICalendar.fromLines(eventText);
+
+      final exdates = iCal.data
+          .where((e) => e.containsKey('exdate'))
+          .map((e) => e['exdate']);
+
+      expect(exdates, [
+        IcsDateTime(dt: "20221210T000000", tzid: "Europe/Berlin"),
+        IcsDateTime(dt: "20221208T000000", tzid: "Europe/Berlin"),
+      ]);
     });
   });
 }
